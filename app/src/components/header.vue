dfd3cdc4f0c06978976135d7e0698dbf8c55fca4
Jonathan Martz <git@jmartz.de>
2022-06-16 17:11:46 +0200

    update header

--- app/src/components/header.vue
+++ app/src/components/header.vue
@@ -2,7 +2,7 @@
   <div class="vc-header flex justify-between mt-2">
     <span
       ><router-link to="/" class="font-bold text-white text-xl ml-2"
-        >www.come-in.rocks</router-link
+        >come-in.rocks</router-link
       ></span
     >
     <nav class="mt-2">
